ae451c2717ea93fa36e0a65c280a68f737c813d1
Jose <jmlb0003@gmail.com>
2018-06-11 20:57:29 +0200

    Added authentication stuff to be able to perform calls against the marvel api

--- app/src/main/java/jmlb0003/com/marveleando/app/utils/ServiceGenerator.java
+++ app/src/main/java/jmlb0003/com/marveleando/app/utils/ServiceGenerator.java
@@ -1,7 +1,26 @@
 package jmlb0003.com.marveleando.app.utils;
 
+import jmlb0003.com.marveleando.BuildConfig;
+import jmlb0003.com.marveleando.data.network.authentication.AuthenticatorInterceptor;
+import okhttp3.OkHttpClient;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public final class ServiceGenerator {
 
-    // Create Retrofit service here
+    private static Retrofit.Builder retrofitBuilder = new Retrofit.Builder()
+            .baseUrl(BuildConfig.MARVEL_API_BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create());
+
+    public static <S> S createService(final Class<S> serviceClass) {
+        final OkHttpClient httpClient = new OkHttpClient.Builder()
+                .addInterceptor(new AuthenticatorInterceptor())
+                .build();
+
+        final Retrofit retrofit = retrofitBuilder
+                .client(httpClient)
+                .build();
+        return retrofit.create(serviceClass);
+    }
 
 }
