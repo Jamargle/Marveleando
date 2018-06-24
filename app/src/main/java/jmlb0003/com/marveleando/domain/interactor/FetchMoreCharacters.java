c89288e520f04a61e95f60ecf7f639594c4961bb
Jose <jmlb0003@gmail.com>
2018-06-24 23:09:12 +0200

    Removed unused dependency in FetchMoreCharacters and check null value of params

--- app/src/main/java/jmlb0003/com/marveleando/domain/interactor/FetchMoreCharacters.java
+++ app/src/main/java/jmlb0003/com/marveleando/domain/interactor/FetchMoreCharacters.java
@@ -10,25 +10,21 @@ import io.reactivex.Observable;
 import io.reactivex.ObservableEmitter;
 import io.reactivex.ObservableOnSubscribe;
 import jmlb0003.com.marveleando.domain.model.Character;
-import jmlb0003.com.marveleando.domain.repository.CharacterLocalRepository;
 import jmlb0003.com.marveleando.domain.repository.CharacterNetworkRepository;
 
 public class FetchMoreCharacters extends UseCase<Integer, List<Character>> {
 
     public static final String NAME = "InjectionKey:FetchMoreCharactersUseCase";
 
-    private final CharacterLocalRepository characterLocalRepository;
     private final CharacterNetworkRepository characterNetworkRepository;
 
     @Inject
     public FetchMoreCharacters(
-            final CharacterLocalRepository characterLocalRepository,
             final CharacterNetworkRepository characterNetworkRepository,
             final ThreadExecutor threadExecutor,
             final PostExecutionThread postExecutionThread) {
 
         super(threadExecutor, postExecutionThread);
-        this.characterLocalRepository = characterLocalRepository;
         this.characterNetworkRepository = characterNetworkRepository;
     }
 
@@ -37,7 +33,11 @@ public class FetchMoreCharacters extends UseCase<Integer, List<Character>> {
         return Observable.create(new ObservableOnSubscribe<List<Character>>() {
             @Override
             public void subscribe(final ObservableEmitter<List<Character>> emitter) {
-                emitter.onNext(characterNetworkRepository.getCharacters(params));
+                if (params == null) {
+                    emitter.onNext(characterNetworkRepository.getCharacters());
+                } else {
+                    emitter.onNext(characterNetworkRepository.getCharacters(params));
+                }
             }
         });
     }
