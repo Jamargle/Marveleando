b90aa7fbef01a50024fc51083b186a2bf4329008
Jose <jmlb0003@gmail.com>
2018-07-14 22:13:10 +0200

    Added toolbar in the details screen to allow up navigation

--- app/src/main/java/jmlb0003/com/marveleando/presentation/detail/CharacterDetailFragment.java
+++ app/src/main/java/jmlb0003/com/marveleando/presentation/detail/CharacterDetailFragment.java
@@ -3,6 +3,8 @@ package jmlb0003.com.marveleando.presentation.detail;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -22,6 +24,7 @@ public final class CharacterDetailFragment
 
     private static final String CHARACTER_TO_SHOW = "CharacterDetailFragment:Character_to_show";
 
+    @BindView(R.id.toolbar) Toolbar toolbar;
     @BindView(R.id.character_image) ImageView characterImageView;
     @BindView(R.id.character_name) TextView characterNameView;
     @BindView(R.id.character_description) TextView characterDescriptionView;
@@ -42,8 +45,18 @@ public final class CharacterDetailFragment
             @Nullable final Bundle savedInstanceState) {
 
         super.onViewCreated(view, savedInstanceState);
+        initToolbar();
         if (getArguments() != null) {
             presenter.loadCharacterDetails((Character) getArguments().getParcelable(CHARACTER_TO_SHOW));
+        }
+    }
+
+    private void initToolbar() {
+        final AppCompatActivity activity = (AppCompatActivity) getActivity();
+        activity.setSupportActionBar(toolbar);
+        if (activity.getSupportActionBar() != null) {
+            activity.getSupportActionBar().setDisplayShowTitleEnabled(false);
+            activity.getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         }
     }
 
