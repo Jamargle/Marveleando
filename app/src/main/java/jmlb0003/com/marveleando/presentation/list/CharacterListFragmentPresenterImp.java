c8e8db5ef81eedb169108d90df829d06892eaf21
Jose <jmlb0003@gmail.com>
2018-07-15 14:44:32 +0200

    Added a new use case to fetch favorite characters and added logic to show them in the list screen

--- app/src/main/java/jmlb0003/com/marveleando/presentation/list/CharacterListFragmentPresenterImp.java
+++ app/src/main/java/jmlb0003/com/marveleando/presentation/list/CharacterListFragmentPresenterImp.java
@@ -8,6 +8,7 @@ import javax.inject.Inject;
 import javax.inject.Named;
 
 import jmlb0003.com.marveleando.domain.interactor.DefaultObserver;
+import jmlb0003.com.marveleando.domain.interactor.FetchFavoriteCharacters;
 import jmlb0003.com.marveleando.domain.interactor.SearchCharacters;
 import jmlb0003.com.marveleando.domain.interactor.UseCase;
 import jmlb0003.com.marveleando.domain.model.Character;
@@ -18,12 +19,15 @@ public final class CharacterListFragmentPresenterImp
         implements CharacterListFragmentPresenter {
 
     private final UseCase<SearchCharacters.Input, List<Character>> searchCharactersUseCase;
+    private final UseCase<Void, List<Character>> fetchFavoriteCharactersUseCase;
 
     @Inject
     public CharacterListFragmentPresenterImp(
-            @Named(SearchCharacters.NAME) final UseCase<SearchCharacters.Input, List<Character>> searchCharactersUseCase) {
+            @Named(SearchCharacters.NAME) final UseCase<SearchCharacters.Input, List<Character>> searchCharactersUseCase,
+            @Named(FetchFavoriteCharacters.NAME) final UseCase<Void, List<Character>> fetchFavoriteCharactersUseCase) {
 
         this.searchCharactersUseCase = searchCharactersUseCase;
+        this.fetchFavoriteCharactersUseCase = fetchFavoriteCharactersUseCase;
     }
 
     @Override
@@ -90,12 +94,35 @@ public final class CharacterListFragmentPresenterImp
 
     @Override
     public void showCharactersWithoutFilters() {
-
+        searchCharacterByName(0, "");
     }
 
     @Override
     public void showOnlyFavorites() {
+        if (getView() != null) {
+            getView().showLoading();
+        }
+        fetchFavoriteCharactersUseCase.execute(
+                null,
+                new DefaultObserver<List<Character>>() {
 
+                    @Override
+                    public void processOnNext(final List<Character> characters) {
+                        if (getView() != null) {
+                            getView().hideNoCharactersToShow();
+                            getView().setBeginningCharactersToShow(characters);
+                            getView().hideLoading();
+                        }
+                    }
+
+                    @Override
+                    public void processOnError(final Throwable exception) {
+                        if (getView() != null) {
+                            getView().hideLoading();
+                            getView().showNoCharactersToShow();
+                        }
+                    }
+                });
     }
 
     @Override
